cd643c16b0bcaaa2f2ce994ceebe53394dc9a6a5
arthan android <kishorec157@gmail.com>
2020-04-10 18:53:50 +0530

    missed 18 issue

--- app/src/main/java/com/example/arthan/dashboard/rm/RMAssignListFragment.kt
+++ app/src/main/java/com/example/arthan/dashboard/rm/RMAssignListFragment.kt
@@ -10,6 +10,7 @@ import androidx.lifecycle.ViewModelProvider
 import com.example.arthan.R
 import com.example.arthan.dashboard.rm.adapters.ReassignAdapter
 import com.example.arthan.dashboard.rm.viewmodel.RMDashboardViewModel
+import com.example.arthan.utils.ProgrssLoader
 import com.example.arthan.views.fragments.BaseFragment
 import kotlinx.android.synthetic.main.assign_list_frag.*
 
@@ -19,8 +20,11 @@ class RMAssignListFragment : BaseFragment() {
     override fun contentView(): Int {
        return R.layout.assign_list_frag
     }
+    var  progressLoader:ProgrssLoader?= null
     private fun loadReassignLeadList(){
         mViewModel.loadReassignLeadList().observe(this, Observer { data->
+
+            progressLoader?.dismmissLoading()
             if(data.isNullOrEmpty()){
                 Toast.makeText(activity,"No Record Found", Toast.LENGTH_SHORT).show()
             } else {
@@ -32,7 +36,8 @@ class RMAssignListFragment : BaseFragment() {
     override fun init() {
 
         mViewModel= ViewModelProvider(this).get(RMDashboardViewModel::class.java)
-
+         progressLoader = ProgrssLoader(context!!)
+        progressLoader!!.showLoading()
         loadReassignLeadList()
     }
 
